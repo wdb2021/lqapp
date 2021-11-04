6100d91c7ebfa79a1607cbe154157f3a27fcf670
wu lq <wdb2021@163.com>
2021-11-04 11:18:51 +0800

    write html hs views urls

---
+++ game/static/js/src/zbase.js
@@ -0,0 +1,7 @@
+class LQGame {
+    constructor(id) {
+    }
+
+}
+
+
---
+++ game/views/index.py
@@ -0,0 +1,6 @@
+//在web端调用，返回html文件
+from django.shortcuts import render
+
+def index(request):
+    return render(request, "multiends/web.html")
+
