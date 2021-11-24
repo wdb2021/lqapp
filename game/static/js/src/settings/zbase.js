570b97987abeb0b468d4ac5f0337006fda54944a
wu lq <wdb2021@163.com>
2021-11-24 15:27:46 +0800

    implement login register

--- game/static/js/src/settings/zbase.js
+++ game/static/js/src/settings/zbase.js
@@ -8,10 +8,106 @@ class Settings {
 
         this.$settings = $(`
 <div class="lq-game-settings" >
+    <div class="lq-game-settings-login">
+        <div class="lq-game-settings-title">
+            登录
+        </div>
+        <div class="lq-game-settings-username">
+            <div class="lq-game-settings-item">
+                <input type="text" placeholder="用户名">
+            </div>
+        </div>
+        <div class="lq-game-settings-password">
+            <div class="lq-game-settings-item">
+                <input type="password" placeholder="密码">
+            </div>
+        </div>
+        <div class="lq-game-settings-submit">
+            <div class="lq-game-settings-item">
+                <button>登录</button>
+            </div>
+        </div>
+        <div class="lq-game-settings-error-message">
 
+
+        </div>
+        <div class="lq-game-settings-option">
+            注册
+        </div>
+        <br>
+        <div class="lq-game-settings-acwing">
+            <img width="30" src="https://lingqin.com.cn/static/image/settings/acwing_logo.png">
+            <br>
+            <div>
+                acwing 一键登录
+            </div>
+        </div>
+
+    </div>
+    <div class="lq-game-settings-register">
+        <div class="lq-game-settings-title">
+            注册
+        </div>
+        <div class="lq-game-settings-username">
+            <div class="lq-game-settings-item">
+                <input type="text" placeholder="用户名">
+            </div>
+        </div>
+        <div class="lq-game-settings-password lq-game-settings-password-first">
+            <div class="lq-game-settings-item">
+                <input type="password" placeholder="密码">
+            </div>
+        </div>
+        <div class="lq-game-settings-password lq-game-settings-password-second">
+            <div class="lq-game-settings-item">
+                <input type="password" placeholder="确认密码">
+            </div>
+        </div>
+        <div class="lq-game-settings-submit">
+            <div class="lq-game-settings-item">
+                <button>注册</button>
+            </div>
+        </div>
+        <div class="lq-game-settings-error-message">
+
+
+        </div>
+        <div class="lq-game-settings-option">
+             登录
+        </div>
+        <br>
+        <div class="lq-game-settings-acwing">
+            <img width="30" src="https://lingqin.com.cn/static/image/settings/acwing_logo.png">
+            <br>
+            <div>
+                acwing 一键登录
+            </div>
+        </div>
+    </div>
 </div>
 
 `);
+        this.$login = this.$settings.find(".lq-game-settings-login");
+        this.$login_username = this.$login.find(".lq-game-settings-username input");
+        this.$login_password = this.$login.find(".lq-game-settings-password input");
+        this.$login_submit = this.$login.find(".lq-game-settings-submit button");
+        this.$login_error_message = this.$login.find(".lq-game-settings-error-message");
+        this.$login_register = this.$login.find(".lq-game-ettings-option");
+        
+
+        this.$login.hide();
+
+        this.$register = this.$settings.find(".lq-game-settings-register");
+        this.$register_username = this.$register.find(".lq-game-settings-username input");
+        this.$register_password = this.$register.find(".lq-game-settings-password-first input");
+        this.$register_password_confirm = this.$register.find(".lq-game-settings-password-second input");
+        this.$register_submit = this.$register.find(".lq-game-settings-submit button");
+        this.$register_error_message = this.$register.find(".lq-game-settings-error-message");
+        this.$register_login = this.$register.find(".lq-game-settings-option");
+
+        this.$register.hide();
+
+
 
         this.root.$lq_game.append(this.$settings);
 
@@ -21,13 +117,38 @@ class Settings {
 
     start() {
         this.getinfo();
+        this.add_listening_events();
     }
 
-    
+    add_listening_events() {
+        let outer = this;
+        this.add_listening_events_login();
+        this.add_listening_events_register();
+    }
+
+    add_listening_events_login() {
+        let outer = this;
+        this.$login_register.click(function() {
+            outer.register();
+        });
+    }
+
+    add_listening_events_register() {
+        let outer = this;
+        this.$register_login.click(function() {
+            outer.login();
+        });
+    }
+
+
     register() {
+        this.$login.hide();
+        this.$register.show();
     }
 
     login() {
+        this.$register.hide();
+        this.$login.show();
 
     }
 
